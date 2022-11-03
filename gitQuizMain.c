7c251e5e514e83eaf2f9dd6a1f7a863db6781311
ExtremeGuelphCoder <ryanmorris989@gmail.com>
2022-11-03 08:58:07 -0400

    this is the message

--- gitQuizMain.c
+++ gitQuizMain.c
@@ -10,6 +10,11 @@ int main(void)
     if(val == 'y'){
         printf("You know how to use Git!");
     }
+        else{
+      printf("you dont know how to use Git");
+
+    }
+        
 
     printf("\n");
 
